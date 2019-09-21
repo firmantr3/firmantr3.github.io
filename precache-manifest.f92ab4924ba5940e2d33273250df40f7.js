fc3946f9c745355063278e7581ae28278254f1f3
Firman Taruna Nugraha <firmantr3@gmail.com>
2019-09-21 15:16:26 +0700

    Update to 1.0.4

---
+++ precache-manifest.f92ab4924ba5940e2d33273250df40f7.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ddebd9b8f0aad97490e",
+    "url": "/css/app.7227ac8d.css"
+  },
+  {
+    "revision": "8ddebd9b8f0aad97490e",
+    "url": "/js/app.98ace13c.js"
+  },
+  {
+    "revision": "5386cbdb76af4353e348",
+    "url": "/css/chunk-0c739f6e.12d22510.css"
+  },
+  {
+    "revision": "5386cbdb76af4353e348",
+    "url": "/js/chunk-0c739f6e.5696fe62.js"
+  },
+  {
+    "revision": "747e909b259fc7d7c237",
+    "url": "/js/chunk-2d0b23b5.530ec338.js"
+  },
+  {
+    "revision": "d53d35efa2b6a3b77cf6",
+    "url": "/css/chunk-45ed8c92.6188dbab.css"
+  },
+  {
+    "revision": "d53d35efa2b6a3b77cf6",
+    "url": "/js/chunk-45ed8c92.5bba6256.js"
+  },
+  {
+    "revision": "b0dba02dac7e99200c5f",
+    "url": "/css/chunk-vendors.90451c50.css"
+  },
+  {
+    "revision": "b0dba02dac7e99200c5f",
+    "url": "/js/chunk-vendors.aa693714.js"
+  },
+  {
+    "revision": "d999eb2d0cd385ef1d50",
+    "url": "/js/lang-en.bd7f95f7.js"
+  },
+  {
+    "revision": "40ed154b8247b738fb2c",
+    "url": "/js/lang-id.6dd80b17.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "97a756a56203fde4b6bd463b7823b3d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "792b3070fc1404296d122b29db60405e",
+    "url": "/icon.svg"
+  },
+  {
+    "revision": "13424f282b89270a9241bdd36e0af1ac",
+    "url": "/img/motaicon.png"
+  },
+  {
+    "revision": "2d3bc41b03a00953e86c3fcac01c1c81",
+    "url": "/img/qytaadmin.png"
+  },
+  {
+    "revision": "fc270f674c1c50ad483f0c29f68e0729",
+    "url": "/img/qytachecklist.png"
+  },
+  {
+    "revision": "397938b0485a5764a0dd85b1f7cc51e1",
+    "url": "/img/qytaadmin.resized.png"
+  },
+  {
+    "revision": "57d5b110a1839101b119d02cee0d682c",
+    "url": "/img/qytahris.png"
+  },
+  {
+    "revision": "70ff02aa1c2791789c052e2495dee0a7",
+    "url": "/img/qytahris.resized.png"
+  },
+  {
+    "revision": "ce3a7a9bb0fd6f37ef3507fb493243bf",
+    "url": "/img/qytasik.resized.png"
+  },
+  {
+    "revision": "ab91fcaa1f326ea1de542f18edbc5d6d",
+    "url": "/img/qytasik.png"
+  },
+  {
+    "revision": "4656fdf1e9a6e68bd3d3b103c3acb2bf",
+    "url": "/img/smanusa.resized.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "0c3d616176101ebce027b8655b79d978",
+    "url": "/img/smanusa.png"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.b0c1651d20ea53480ec35b25fa538c3d.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.f92ab4924ba5940e2d33273250df40f7.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
