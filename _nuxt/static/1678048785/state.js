caf0d857a7e7eceb2c5790bc619186e86b15fdd9
Firman Taruna Nugraha <firmantr3@gmail.com>
2023-03-06 03:43:54 +0700

    migrate to nuxt (v2)

---
+++ _nuxt/static/1678048785/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c,d){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1678048785",layout:"default",error:a,state:{lang:{locale:"en",locales:{id:"ID",en:"EN"},localeIsSet:b},ui:{shrink:b,sidebar:c},i18n:{routeParams:{}}},serverRendered:c,routePath:d,config:{_app:{basePath:d,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,false,true,"\u002F"));
