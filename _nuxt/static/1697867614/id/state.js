8a2817962b36b9e3cb44d41fc5bfc3c3138d4434
Firman Taruna Nugraha <firmantr3@outlook.com>
2023-10-21 13:02:27 +0700

    update GA

--- _nuxt/static/1697867614/id/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1697867614",layout:"default",error:a,state:{lang:{locale:"id",locales:{id:"ID",en:"EN"},localeIsSet:b},ui:{shrink:b,sidebar:c},i18n:{routeParams:{}}},serverRendered:c,routePath:"\u002Fid",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,false,true));
